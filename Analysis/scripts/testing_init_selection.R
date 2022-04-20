b449e8de741b72e57dfcded637bb5f15188453e3
Alasdair Clarke <a.clarke@essex.ac.uk>
2022-04-20 16:15:57 +0100

    blip

--- Analysis/scripts/testing_init_selection.R
+++ Analysis/scripts/testing_init_selection.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(rstan)
 library(tidybayes)
+library(truncnorm)
 
 options(digits = 2, mc.cores = 10)
-d <- read_csv("data/clarke_2020_qjep.csv") %>%
+d <- read_csv("../data/clarke_2020_qjep.csv") %>%
   filter(found == 1)
 
 
@@ -13,7 +14,7 @@ stan_data <- list(N = nrow(d),
                   x = d$x,
                   y = d$y)
 
-m <- stan("models/initial_selection_models/init_sel.stan",
+m <- stan("../models/initial_selection_models/init_sel.stan",
                 data = stan_data,
                 chains = 4,
                 iter = 1000,
@@ -21,24 +22,42 @@ m <- stan("models/initial_selection_models/init_sel.stan",
 
 
 
-saveRDS(m, "scratch/init_sel_model.rds")
+saveRDS(m, "../scratch/init_sel_model.rds")
+
+m <- readRDS("../scratch/init_sel_model.rds")
 
 gather_draws(m, u[dim, observer]) %>% 
   ungroup() %>%
   select(-.variable) %>%
-  rename(obs_mu = ".value") %>%
-  full_join(gather_draws(m, mu[dim])) %>%
-  mutate(obs_mu = .value + obs_mu) -> a 
+  rename(obs_b = ".value") %>%
+  full_join(gather_draws(m, b[dim])) %>%
+  mutate(obs_b = .value + obs_b,
+         .variable = if_else(dim %in% c(1,2), "mu", "sd"),
+         dim = if_else(dim %in% c(1,3), "x", "y")) %>%
+  select(observer, dim, .iteration, obs_b, param = ".variable") %>%
+  mutate(obs_b = if_else(param == "sd", exp(obs_b), obs_b)) -> a 
+
+ggplot(a, aes(obs_b, group = observer)) + 
+  geom_density(alpha = 0.25) +
+  facet_grid(param ~ dim, scales = "free")
+ 
+a %>% group_by(observer, param, dim) %>%
+  summarise(value = mean(obs_b) , .groups = "drop") %>%
+  pivot_wider(names_from = param, values_from = value) %>%
+  rename(mean = "mu") -> am
+
+obs_fits_x <- pmap_dfc(filter(am,  dim == "x") %>% select(mean, sd), dtruncnorm, a = 0, b= 1, x = z) %>%
+  mutate(z = z) %>%
+  pivot_longer(-z, names_to = "observer", values_to = "v") %>%
+  ggplot(aes(x = z, y = v, group = observer)) + geom_path() +
+  scale_x_continuous("x")
+
+obs_fits_y <- pmap_dfc(filter(am,  dim == "y") %>% select(mean, sd), dtruncnorm, a = 0, b= 1, x = z) %>%
+  mutate(z = z) %>%
+  pivot_longer(-z, names_to = "observer", values_to = "v") %>%
+  ggplot(aes(x = z, y = v, group = observer)) + geom_path() +
+  scale_x_continuous("y")
 
 
-d %>% pivot_longer(c(x, y), names_to = "dim", values_to = "value") %>%
-  mutate(dim = if_else(dim == "x", 1, 2)) -> d_plt
+obs_fits_x + obs_fits_y
 
-a %>% ggplot(aes(obs_mu, group = observer)) + geom_density() + 
-  facet_grid(observer~dim, scales = "free") + 
-  geom_density(data = d_plt, aes(value), colour = "purple")
-
-%>%
-  ggplot(aes(.value, group = observer)) + 
-  geom_density(alpha = 0.1) + 
-  facet_wrap(~.variable)
